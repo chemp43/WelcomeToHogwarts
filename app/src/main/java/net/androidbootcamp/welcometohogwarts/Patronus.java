d4e6c08bafd6ef0366f71ff66178703229d43bc9
Curtis Hempstead <33561751+chemp43@users.noreply.github.com>
2018-04-14 15:41:44 -0600

    Added Distance Activity and strings

--- app/src/main/java/net/androidbootcamp/welcometohogwarts/Patronus.java
+++ app/src/main/java/net/androidbootcamp/welcometohogwarts/Patronus.java
@@ -1,13 +1,27 @@
 package net.androidbootcamp.welcometohogwarts;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Patronus extends AppCompatActivity {
+    Button distance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patronus);
+        distance=(Button)findViewById(R.id.btnDistance);
+        distance.setOnClickListener(btnDistance);
     }
+    Button.OnClickListener btnDistance = new Button.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            startActivity(new Intent(Patronus.this, Distance.class));
+
+        }
+    };
+
 }
