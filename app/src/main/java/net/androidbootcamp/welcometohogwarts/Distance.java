825a8f7d68b121d41d5584d07bcb4b3c059c21a1
Curtis Hempstead <33561751+chemp43@users.noreply.github.com>
2018-05-06 16:04:00 -0600

    Got Maps to work with a Marker to the platform!

--- app/src/main/java/net/androidbootcamp/welcometohogwarts/Distance.java
+++ app/src/main/java/net/androidbootcamp/welcometohogwarts/Distance.java
@@ -2,12 +2,52 @@ package net.androidbootcamp.welcometohogwarts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class Distance extends AppCompatActivity {
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+/**
+ * An activity that displays a Google map with a marker (pin) to indicate a particular location.
+ */
+public class Distance extends AppCompatActivity
+        implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Retrieve the content view that renders the map.
         setContentView(R.layout.activity_distance);
+        // Get the SupportMapFragment and request notification
+        // when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map when it's available.
+     * The API invokes this callback when the map is ready to be used.
+     * If Google Play services is not installed on the device, the user receives a prompt to install
+     * Play services inside the SupportMapFragment. The API invokes this method after the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        // Add a marker in Sydney, Australia,
+        // and move the map's camera to the same location.
+        LatLng sydney = new LatLng(51.5316, 0.1244);
+        googleMap.addMarker(new MarkerOptions().position(sydney)
+                .title("Platform 9 and three-quarters"));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
